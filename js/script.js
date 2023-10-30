cc221afc76bfe6836254163dad428c7bd7bc16e9
TPCodo <cecimartignoni@gmail.com>
2023-10-30 09:46:05 -0300

    con funcionalidad JS

--- js/script.js
+++ js/script.js
@@ -1,72 +1,55 @@
 
-//variable: espacio reservado de la memoria. Para guardar datos y resultados en la memoria (de la pc durante dura la página)
-
-//VARIABLES. Dos formas: let y var   siempre let (porque es scope local)
-//let?
 
 
-/*
-
-  let valorMaquina="piedra"
-  let valorPersona=""
-
-contador=0;
-contadorPersona=0
-contadorMaquina=0
-
-while (contador<3){
-
-  while (true) {
-    valorPersona = prompt("¿Piedra, papel y tijera?");
-   valorPersona= valorPersona.toLowerCase();   
-  
-    if ((valorPersona) === "piedra") {
-      contador++;
-      break; // Sal del bucle 
-    } else if (valorPersona === "papel") {
-      contadorPersona++
-      contador++;
-      break; // Sal del bucle 
-    } else if (valorPersona === "tijera") {
-        contadorMaquina++
-        contador++;
-        break; // Sal del bucle 
-    } else {
-      alert("El valor ingresado no coincide con ninguno de los tres posibles valores. Inténtalo de nuevo.");
-    }
-  }  
-}
-
-
-    if(contadorMaquina>contadorPersona){
-        alert("Ganó la máquina");}
-    else if (contadorMaquina<contadorPersona){
-        alert("Ganaste")}
-    else{
-        alert("Empataron")}
-
-
-        function saludarConNombre(nombre){
-            console.log("hola", nombre);
-          }
-
-          const duplicar=(a)=>a*2;
-
-          console.log(duplicar(5))
-
-*/
-
-
-        var inputElement = document.getElementById("form_name");
+        var inputName = document.getElementById("form_name");
+        var inputLastName = document.getElementById("form_last_name");
+        var inputEmail = document.getElementById("form_email");
+        var inputCantidad = document.getElementById("form_quantity");        
+        var selectCategoria = document.getElementById("form_selection");
         var boton = document.getElementById("boton_resumen");
-
-      
         var valorName;
-        var resultadoElement = document.getElementById("resultado");
+        var valorLastName;
+        var valorEmail;
+        var valorCantidad;
+        var valorCategoria;
+
    
         boton.addEventListener("click", function() {
-          valorName = inputElement.value; // Guarda el valor del input en la variable
-          resultadoElement.textContent="El valor del input es: " + valorName;
+          valorName = inputName.value; // Guarda el valor del input en la variable
+          valorLastName = inputLastName.value;
+          valorEmail = inputEmail.value;
+          valorCantidad = inputCantidad.value;
+          valorCategoria = selectCategoria.value;
+          console.log(valorName, valorLastName, valorEmail, valorCantidad, valorCategoria);
+
+var valorCantidadXCategoria;
+  
+  if (valorCategoria === "1"){
+    valorCantidadXCategoria = valorCantidad * 40;
+
+    } else if(valorCategoria ==="2"){
+      valorCantidadXCategoria = valorCantidad* 100;
+
+    } else{
+      valorCantidadXCategoria = valorCantidad * 170;
+
+    }
+
+
+    
+          mensajeJS.textContent = "Total a pagar $ " + valorCantidadXCategoria;
+    
+
         });
-      
-   
+
+
+        var boton_borrar = document.getElementById("boton_borrar");
+
+        boton_borrar.addEventListener("click", function() {
+          inputName.value = "";
+          inputLastName.value = "";
+          inputEmail.value = "";
+          inputCantidad.value = "";
+          selectCategoria.value = "";
+          mensajeJS.textContent = "Total a pagar $ ";
+        });
